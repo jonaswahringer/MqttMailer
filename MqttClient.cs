24cbbdefba9161afae7ab09082c60a88460ae0c4
Jonas Wahringer <jonas.wahringer@gmail.com>
2022-06-03 15:27:43 +0200

    working client

---
+++ MqttClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQTTDemo
+{
+    class MqttClient
+    {
+        private IMqttFactory factory = new MqttFactory();
+        private IMqttClient? mqttClient;
+
+        public async void Init(string host, int port, string clientId, string email)
+        {
+            mqttClient = factory.CreateMqttClient();
+
+            var options = new MqttClientOptionsBuilder()
+                .WithClientId(clientId)
+                .WithTcpServer(host, port)
+                .WithCleanSession()
+                .Build();
+
+            mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine($"{DateTime.Now} " +
+                    $"{e.ApplicationMessage.Topic}; " +
+                    $"{Encoding.UTF8.GetString(e.ApplicationMessage.Payload)};");
+
+                Task.Run(() => mqttClient.PublishAsync("topic/*", "refresh"));
+
+                if(!String.IsNullOrEmpty(email))
+                {
+                    // Send Email
+                    SendEmail(email);
+                }
+            });
+
+            mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                // Subscribe to a topic
+                await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("topic/rechnung").Build());
+                await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("topic/position").Build());
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+        }
+
+        private void SendEmail(string destination)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandLine;
+
+
+namespace MQTTDemo
+{
+    class Program
+    {
+        private static string MqttHost { get; set; } = string.Empty;
+        private static int MqttPort { get; set; } = 0;
+        private static string MqttClientId { get; set; } = string.Empty;
+        private static string MqttDestinationEmail { get; set; } = string.Empty;
+
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+            [Option('h', "host", Required = true, HelpText = "Set Host for MQTT Client")]
+            public string Host { get; set; }
+            [Option('p', "port", Required = true, HelpText = "Specify Port number for MQTT Client")]
+            public int Port { get; set; }
+            [Option('c', "client", Required = true, HelpText = "Set Client ID for MQTT Client")]
+            public string Client{ get; set; }
+            [Option('e', "email", Required = false, HelpText = "Set Destination Address to recieve MQTT Data over Email")]
+            public string Email { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       if(o.Host.Length != 0)
+                       {
+                           Console.WriteLine(o.Host);
+                           MqttHost = o.Host;
+                       }
+                       if (o.Port != 0)
+                       {
+                           Console.WriteLine(o.Port);
+                           MqttPort = o.Port;
+                       }
+                       if (o.Client.Length != 0)
+                       {
+                           Console.WriteLine(o.Client);
+                           MqttClientId = o.Client;
+                       }
+                       if(!String.IsNullOrEmpty(o.Email))
+                       {
+                           Console.WriteLine(o.Email);
+                           MqttDestinationEmail = o.Email;
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
+            
+            Console.WriteLine(MqttPort);
+            Console.WriteLine(MqttPort.GetType());
+
+            MqttClient mqttClient = new MqttClient();
+            mqttClient.Init(MqttHost, MqttPort, MqttClientId, MqttDestinationEmail);
+
+            Console.Read();
+        }
+    }
+}
